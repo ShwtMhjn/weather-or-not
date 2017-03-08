b6799697d50dfbc0c2d3048e56f1df98e6759f97
Shweta Mahajan <shweta_mahajan1987@yahoo.com>
2017-03-08 16:20:52 +0530

    Table View Cell selection changed

--- WeatherOrNot/ViewController.m
+++ WeatherOrNot/ViewController.m
@@ -152,6 +152,9 @@
     return cell;
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
 
 
 @end
